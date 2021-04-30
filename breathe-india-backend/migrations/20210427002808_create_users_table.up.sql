e296e5e86cb73955bc40dcf769d0285f3745626a
Farseen <zah4n5dz@anonaddy.me>
2021-04-30 11:11:58 +0530

    posts table migrations

--- breathe-india-backend/migrations/20210427002808_create_users_table.up.sql
+++ breathe-india-backend/migrations/20210427002808_create_users_table.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 
 CREATE TABLE users (
-    id UUID NOT NULL DEFAULT gen_random_uuid(),
+    id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     name TEXT NOT NULL,
     email TEXT NOT NULL,
     profile_pic_url TEXT NOT NULL,
---
+++ breathe-india-backend/migrations/20210430051540_posts_table.up.sql
@@ -0,0 +1,21 @@
+-- Add up migration script here
+CREATE TYPE PostType AS ENUM ('needs', 'supplies');
+
+CREATE TABLE posts (
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    userid uuid NOT NULL REFERENCES users(id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    post_type PostType NOT NULL,
+    state TEXT NOT NULL,
+    district TEXT NOT NULL,
+    city TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    message TEXT NOT NULL
+);
+
+CREATE TABLE post_items (
+    id uuid NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    post_id uuid NOT NULL REFERENCES posts(id) ON UPDATE RESTRICT ON DELETE CASCADE,
+    item TEXT NOT NULL,
+    quantity TEXT NOT NULL
+);
