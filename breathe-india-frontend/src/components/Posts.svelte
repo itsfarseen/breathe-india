5a9a1c6e341da51e644db2f5a41a0057272edbd5
Farseen <zah4n5dz@anonaddy.me>
2021-05-01 22:42:05 +0530

    post listing with relative time

--- breathe-india-frontend/src/components/Posts.svelte
+++ breathe-india-frontend/src/components/Posts.svelte
@@ -1,6 +1,17 @@
 <script>
+  import TimeAgo from "javascript-time-ago";
+
+  import api from "../api";
   import { navigate } from "svelte-routing";
+  import Post from "./Post.svelte";
+
   export let type = "";
+
+  let posts = api.getPosts({ typ: type });
+
+  // @ts-ignore
+
+  const timeAgo = new TimeAgo("en-US");
 </script>
 
 <div class="flex flex-col">
@@ -29,6 +40,32 @@
         />
       </label>
     </div>
+  </div>
+  <div class="flex-1 flex flex-col divide-y divide-gray-300">
+    {#await posts}
+      <h1
+        class="text-2xl mt-16 text-center font-bold text-gray-500 animate-pulse"
+      >
+        Loading ..
+      </h1>
+    {:then posts}
+      {#each posts.posts as post}
+        <div class="p-4 flex gap-4 active:bg-gray-100">
+          <div class="flex flex-col flex-1">
+            <div class="text-lg font-medium text-gray-600">{post.item}</div>
+            <div class="text-sm text-gray-600">
+              {post.quantity} at {post.spot}
+            </div>
+            <div class="text-sm text-gray-600">
+              {[post.city, post.district, post.state].join(", ")}
+            </div>
+          </div>
+          <div class="text-gray-500">
+            {timeAgo.format(new Date(post.updated_at), "mini")}
+          </div>
+        </div>
+      {/each}
+    {/await}
   </div>
 </div>
 
--- breathe-india-frontend/src/main.js
+++ breathe-india-frontend/src/main.js
@@ -1,6 +1,11 @@
 import "./global.postcss"
 import App from './App.svelte'
 
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+// put here cuz must be called only once.
+TimeAgo.addDefaultLocale(en);
+
 const app = new App({
   target: document.getElementById('app')
 })
