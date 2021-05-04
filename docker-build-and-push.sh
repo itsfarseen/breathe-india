01f8fb01e7d39f9bbeee8539dd8b458d2221a596
Farseen <zah4n5dz@anonaddy.me>
2021-05-04 13:54:42 +0530

    script to automate building, connecting to private registry and pushing the changes to my server

---
+++ docker-build-and-push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Configuration, change as needed
+SSH_USER=farzeen
+SSH_HOST=bisket-town
+
+docker-compose build
+
+# -M put into master mode so that it can be controlled through a socket even if
+# put in background.
+# -S specify the control socket
+# -f put in background.
+# -N Don't run any commands, just forward.
+# -T Don't allocate tty
+ssh -M -S ./ssh-tunnel-ctrl-socket -fNT -L 5000:localhost:5000 $SSH_USER@$SSH_HOST
+
+docker-compose push
+
+# Stop ssh tunnelling
+ssh -S ./ssh-tunnel-ctrl-socket -O exit $SSH_USER@$SSH_HOST
