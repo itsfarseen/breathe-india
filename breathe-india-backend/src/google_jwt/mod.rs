cf159c4b73673462b984d2976dcddf8a2e7f7351
Gokul Soumya <gokulps15@gmail.com>
2021-05-07 21:45:56 +0530

    Remove JwkConfiguration, superceded by JwtVerifier

--- breathe-india-backend/src/google_jwt/mod.rs
+++ breathe-india-backend/src/google_jwt/mod.rs
@@ -1,9 +1,7 @@
 // Adapted from: https://medium.com/@maylukas/firebase-token-authentication-in-rust-a1885f0982df
 
-mod config;
 mod fetch_keys;
 mod verify;
 
-pub use config::JwkConfiguration;
 pub use fetch_keys::{JwkKey, JwkKeys};
 pub use verify::{Claims, JwtVerifier, VerificationError};
